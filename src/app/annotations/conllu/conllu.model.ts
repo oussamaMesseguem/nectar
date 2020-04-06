659422cede999da74890ce7476b5d80ab98cd38a
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-06 15:44:31 +0200

    annotations will not be modules atm
    better have all the definitions of annot in one file

--- src/app/annotations/conllu/conllu.model.ts
+++ src/app/annotations/conllu/conllu.model.ts
@@ -86,3 +86,56 @@ export class ConllTokenForm {
         this.misc.setValue(conll.misc);
     }
 }
+
+export interface UPos {
+    tag: string;
+    name: string;
+    definition: string;
+    link: string;
+    examples: string[];
+}
+
+export interface XPos {
+    tag: string;
+    name: string;
+    definition: string;
+    link: string;
+    examples: string[];
+}
+export interface UDeprel {
+    tag: string;
+    name: string;
+    link: string;
+}
+
+export interface UFeats {
+    tag: string;
+    name: string;
+    link: string;
+    values: Feat[];
+}
+
+interface Feat {
+    tag: string;
+    name: string;
+    link: string;
+}
+
+export interface UDeps {
+    head: number[];
+    values: UDeprel[];
+}
+
+interface DepsPair {
+    index: number;
+    deprel: UDeprel;
+}
+
+export interface ConlluTags {
+    upos: UPos[];
+    xpos: XPos[];
+    feat: UFeats[];
+    head: number[];
+    deprel: UDeprel[];
+    deps: UDeps;
+}

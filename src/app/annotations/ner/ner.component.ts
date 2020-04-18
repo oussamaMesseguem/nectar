985d76ba2c800cc255ceafdeab074cea635f1dec
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-18 13:32:52 +0200

    Unsubscribe on onDestroy

--- src/app/annotations/ner/ner.component.ts
+++ src/app/annotations/ner/ner.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Ner } from './ner.service';
 import { NerToken } from './ner.model';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ner',
   templateUrl: './ner.component.html',
   styleUrls: ['./ner.component.scss']
 })
-export class NerComponent implements OnInit {
+export class NerComponent implements OnInit, OnDestroy {
 
   sentence: NerToken[];
+
+  private ondestroy$: Subject<any> = new Subject();
 
   constructor(private nerService: Ner) { }
 
   ngOnInit(): void {
-    this.nerService.sentence$.subscribe(sentence => this.sentence = sentence);
+    this.nerService.sentence$.pipe(takeUntil(this.ondestroy$)).subscribe(sentence => this.sentence = sentence);
+  }
+
+  /**
+   * Need to unsubscribe when the annoatation view changes
+   */
+  ngOnDestroy(): void {
+    this.ondestroy$.next();
   }
 
   @Input() set currentSentenceIndex(sentenceIndex: number) {
-    console.log(sentenceIndex);
-
     this.nerService.moveSentence(sentenceIndex);
   }
 

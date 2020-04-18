26fd2e7a7f4cd95df7f91091b1c0e15fc3cdf0e8
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-18 14:38:50 +0200

    Subcribe with async
    The subscribtion is done by asyn in html as well as unsub
    Therefore no need to unsubscribe manually

--- src/app/annotations/ner/ner.component.ts
+++ src/app/annotations/ner/ner.component.ts
@@ -1,32 +1,23 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Ner } from './ner.service';
 import { NerToken } from './ner.model';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-ner',
   templateUrl: './ner.component.html',
   styleUrls: ['./ner.component.scss']
 })
-export class NerComponent implements OnInit, OnDestroy {
+export class NerComponent implements OnInit {
 
-  sentence: NerToken[];
-
-  private ondestroy$: Subject<any> = new Subject();
+  sentence$: Observable<NerToken[]>;
 
   constructor(private nerService: Ner) { }
 
   ngOnInit(): void {
-    this.nerService.sentence$.pipe(takeUntil(this.ondestroy$)).subscribe(sentence => this.sentence = sentence);
+    this.sentence$ = this.nerService.sentence$;
   }
 
-  /**
-   * Need to unsubscribe when the annoatation view changes
-   */
-  ngOnDestroy(): void {
-    this.ondestroy$.next();
-  }
 
   @Input() set currentSentenceIndex(sentenceIndex: number) {
     this.nerService.moveSentence(sentenceIndex);

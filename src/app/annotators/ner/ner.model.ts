2568e8b8e558cbeb1ee86826d201ff2c0646d876
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-23 16:23:28 +0200

    By default ner have 'O' as tags
    function to create the text

--- src/app/annotators/ner/ner.model.ts
+++ src/app/annotators/ner/ner.model.ts
@@ -16,12 +16,42 @@ export class NerParser implements IParser {
     constructor() {}
 
     ofToken(value: string[]): NerToken {
-        return { token: value[0], tag: value[1], type: value[2]};
+        const token = value[0];
+        let tag = 'O';
+        let type = '';
+        if (value[1] !== 'O') {
+            const t = value[1].split('-');
+            tag = t[0];
+            type = t[1];
+        }
+        return { token, tag, type };
     }
 }
 
 export function createNerToken(token: string, tag?: string, type?: string) {
     return { token, tag: tag ? tag : '', type: type ? type : '' };
+}
+
+/**
+ * Appends the content into a string.
+ * @param content the content from the store
+ */
+export function nerIntoText(content: NerToken[][]): string {
+    const text = [];
+    content.forEach(sentence => {
+        const sentenceArray = [];
+        sentence.forEach(token => {
+            const values: string[] = Object.values(token);
+            let tag = 'O';
+            if (values[1] !== 'O') {
+                tag = values[1].concat('-', values[2]);
+            }
+            values.splice(1, 2, tag);
+            sentenceArray.push(values.join('\t'));
+        });
+        text.push(sentenceArray.join('\n'));
+    });
+    return text.join('\n\n');
 }
 
 export const NerTags = [

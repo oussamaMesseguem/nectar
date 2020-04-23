23c378268cdb944c382627679cef70acfd48f588
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-23 16:27:30 +0200

    Value unused and new title

--- src/app/editor/export/export.component.ts
+++ src/app/editor/export/export.component.ts
@@ -13,24 +13,33 @@ export interface ExportDataDialog {
 export class ExportComponent implements OnInit {
 
   exportedAnnotations: { annotation: string; selected: boolean }[] = [];
-  value = '';
 
   constructor(
     public dialogRef: MatDialogRef<ExportComponent>,
     @Inject(MAT_DIALOG_DATA) public exportDataDialog: ExportDataDialog,
     public dialog: MatDialog) { }
 
+  /**
+   * At init each annotations are displayed and not selected by default.
+   */
   ngOnInit(): void {
     this.exportDataDialog.annotations.forEach(annotation => {
       this.exportedAnnotations.push({ annotation, selected: false});
     });
   }
 
+  /**
+   * Marks the annotation as selected.
+   * @param index The index of the selected annotation
+   */
   touch(index: number) {
     this.exportedAnnotations[index].selected = !this.exportedAnnotations[index].selected;
   }
 
+  /**
+   * Returns the selected annotations.
+   */
   export(): string[] {
-    return this.exportedAnnotations.map(a => a.annotation);
+    return this.exportedAnnotations.filter(a => a.selected).map(a => a.annotation);
   }
 }

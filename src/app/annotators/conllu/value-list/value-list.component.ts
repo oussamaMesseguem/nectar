5eb0b9fddec27a992a2fb29147ee0102705e675e
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-30 13:38:36 +0200

    Unused import

--- src/app/annotators/conllu/value-list/value-list.component.ts
+++ src/app/annotators/conllu/value-list/value-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormArray } from '@angular/forms';
 
 
 /**
@@ -36,8 +35,7 @@ export class ValueListComponent implements OnInit {
   fields: string[][] = [];
 
   constructor(public dialogRef: MatDialogRef<ValueListComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: DialogData,
-              private fb: FormBuilder) { }
+              @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
 

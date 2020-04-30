944647e9dcc1b68a3fa448ad38013da6ea6be74e
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-04-30 13:37:19 +0200

    Tests for value list componenent

--- src/app/annotators/conllu/value-list/value-list.component.spec.ts
+++ src/app/annotators/conllu/value-list/value-list.component.spec.ts
@@ -82,4 +82,32 @@ describe('ValueListComponent', () => {
     component.updateField('AdpType', 1, 0);
     expect(component.fields[1]).toEqual(['AdpType', 'erg'], 'after update the second element should have AdpType as tag.');
   });
+
+  it('should format the field array by concatenation using the separator and equality signs', () => {
+    component.data = { tag: 'Abbr=Yes|AbsErgDatNumber=abs', tags, equality: '=', separator: '|' };
+    component.ngOnInit();
+    expect(component.fields).toEqual([['Abbr', 'Yes'], ['AbsErgDatNumber', 'abs']], 'the tag has been split into values at init.');
+    component.updateField('erg', 1, 1);
+    const expectedTag = 'Abbr=Yes|AbsErgDatNumber=erg';
+    expect(component.format()).toEqual(expectedTag, 'format should join the array using separator and equality signs.');
+  });
+
+  it('should format the field array and ignore empty values', () => {
+    component.data = { tag: 'Abbr=Yes|AbsErgDatNumber=abs', tags, equality: '=', separator: '|' };
+    component.ngOnInit();
+    expect(component.fields).toEqual([['Abbr', 'Yes'], ['AbsErgDatNumber', 'abs']], 'the tag has been split into values at init.');
+    const addButtonElt = fixture.debugElement.query(By.css('button'));
+    addButtonElt.triggerEventHandler('click', null);
+    expect(component.fields).toEqual([['Abbr', 'Yes'], ['AbsErgDatNumber', 'abs'], []], 'the third array should be empty.');
+    const expectedTag = 'Abbr=Yes|AbsErgDatNumber=abs';
+    expect(component.format()).toEqual(expectedTag, 'the empty array should have been ignored in the format.');
+  });
+
+  it('should format single values without equality sign', () => {
+    component.data = { tag: 'Abbr=Yes|AbsErgDatNumber', tags, equality: '=', separator: '|' };
+    component.ngOnInit();
+    expect(component.fields).toEqual([['Abbr', 'Yes'], ['AbsErgDatNumber']], 'the tag has been split into values at init.');
+    const expectedTag = 'Abbr=Yes|AbsErgDatNumber';
+    expect(component.format()).toEqual(expectedTag, 'the single value should not contain equality sign.');
+  });
 });

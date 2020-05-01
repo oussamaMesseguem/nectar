0f264c531845f294c8f3c12faa8b975c8e4870fe
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-05-01 18:03:01 +0200

    Export component unit tests

--- src/app/export/export.component.spec.ts
+++ src/app/export/export.component.spec.ts
@@ -2,6 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExportComponent } from './export.component';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Annotation } from '../annotators/annotations';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+const annotations: string[] = Object.values(Annotation);
 
 describe('ExportComponent', () => {
   let component: ExportComponent;
@@ -12,7 +17,7 @@ describe('ExportComponent', () => {
       imports: [ MatDialogModule ],
       providers: [
         { provide: MatDialogRef, useValue: {} },
-        { provide: MAT_DIALOG_DATA, useValue: { annotations: [] } }
+        { provide: MAT_DIALOG_DATA, useValue: { annotations } }
       ],
       declarations: [ ExportComponent ]
     })
@@ -22,10 +27,28 @@ describe('ExportComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExportComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set selected to false in ngOnint', () => {
+    component.ngOnInit();
+    expect(component.exportedAnnotations.map(a => a.selected)).toEqual(Array(annotations.length).fill(false));
+  });
+
+  it('should change selected to true when touched', () => {
+    component.ngOnInit();
+    component.touch(1);
+    expect(component.exportedAnnotations[1].selected).toBeTruthy('annotation should be true after click');
+  });
+
+  it('should export only the selected to true annotations', () => {
+    component.ngOnInit();
+    component.touch(0);
+    const touched: string[] = component.export();
+    expect(touched).toEqual([component.exportedAnnotations[0].annotation], 'annotations should be the ones that are true');
+
   });
 });

0b7bbd5472b8e5bd11ae8681f36babf1d534fad5
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-05-02 02:05:34 +0200

    Adjustor is not a dialog anymore

--- src/app/adjustor/adjustor/adjustor.component.ts
+++ src/app/adjustor/adjustor/adjustor.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AdjustorService } from '../adjustor.service';
-import { MatDialogRef } from '@angular/material/dialog';
 
 
 @Component({

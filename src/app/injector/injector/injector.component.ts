5f15b2cb129bbc0de2db78e56fa93c5064c3ad6c
Oussama Messeguem <oussama.messeguem@gmail.com>
2020-05-04 15:04:14 +0200

    Injector remove unused methods and some refactoring

--- src/app/injector/injector/injector.component.ts
+++ src/app/injector/injector/injector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Annotation, Language } from '../../annotators/annotations';
@@ -20,9 +20,18 @@ import { InjectorService } from '../injector.service';
 })
 export class InjectorComponent implements OnInit {
 
+  /**
+   * The accepted type of files
+   */
+  annotations: string[] = Object.values(Annotation);
+  /**
+   * The accepted languages
+   */
   languages: string[] = Object.values(Language);
-  types: string[] = Object.values(Annotation);//.filter(a => a !== Annotation.raw);
 
+  /**
+   * The form group to upload content
+   */
   formGroup: FormGroup = this.formBuilder.group(new ContentUploadForm());
 
   /**
@@ -31,6 +40,9 @@ export class InjectorComponent implements OnInit {
    */
   contentValidationInProgress = false;
 
+  /**
+   * Array of uploaded files
+   */
   files: Array<FileUploadModel> = [];
 
   constructor(
@@ -39,17 +51,50 @@ export class InjectorComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  /**
-   * Whether the selected type is Raw.
-   * The goto buttons depens on the types
-   */
-  get isRaw(): boolean {
-    return this.type.value === Annotation.raw;
+  get lang(): FormControl | null { return this.formGroup.get(ContentUploadForm.LANG) as FormControl; }
+  get annotation(): FormControl | null { return this.formGroup.get(ContentUploadForm.ANNOTATION) as FormControl; }
+
+  cancelFile() {
+    // Stop the parsing of the content
+    this.injectionService.cancelContentInjection();
+    // No file uploaded, hence empty array
+    this.files = [];
+    // No parsing running
+    this.contentValidationInProgress = false;
   }
 
-  get lang(): FormControl | null { return this.formGroup.get(ContentUploadForm.LANG) as FormControl; }
-  get type(): FormControl | null { return this.formGroup.get(ContentUploadForm.TYPE) as FormControl; }
+  /**
+   * The content get parsed and upload only when the language and the annotation are set
+   * @param file The upload content
+   */
+  onFileComplete(file: FileUploadModel) {
+    if (this.formGroup.valid) {
+      const reader = new FileReader();
+      reader.readAsText(file.data);
 
+      // While on progress a progress bar is displayed
+      reader.onprogress = (event: ProgressEvent) => {
+        file.progress = Math.round((event.loaded * 100) / event.total);
+      };
+
+      // Once the content has been read, it's sent to the service
+      // for parsing.
+      reader.onload = _ => {
+        this.injectionService.injectContent(this.lang.value, this.annotation.value, reader.result.toString())
+          .subscribe(
+            next => {
+              this.contentValidationInProgress = next;
+            },
+            error => {
+              this.contentValidationInProgress = false;
+              console.error(`An error occured while parsing the content: ${error}`);
+            },
+            () => {
+              this.contentValidationInProgress = false;
+            });
+      };
+    }
+  }
 
   onUpload() {
     const fileUpload = document.getElementById(
@@ -57,7 +102,6 @@ export class InjectorComponent implements OnInit {
     ) as HTMLInputElement;
 
     fileUpload.onchange = () => {
-
       Array.from(fileUpload.files).forEach(file => {
         this.files.push({
           data: file,
@@ -76,75 +120,25 @@ export class InjectorComponent implements OnInit {
     const fileUpload = document.getElementById(
       'fileUpload'
     ) as HTMLInputElement;
+
     fileUpload.value = '';
 
     this.files.forEach(file => {
       this.onFileComplete(file);
     });
   }
-
-  /**
-   * Tells whether the annoation is raw, if so to the adjusment form will be displayed.
-   */
-  adjust(): boolean {
-    return this.isRaw;
-  }
-
-  /**
-   * The content get parsed and upload only when the language and the type are set
-   * @param file The upload content
-   */
-  onFileComplete(file: FileUploadModel) {
-    if (this.formGroup.valid) {
-      const reader = new FileReader();
-      reader.readAsText(file.data);
-
-      // While on progress a progress bar is displayed
-      reader.onprogress = (event: ProgressEvent) => {
-        file.progress = Math.round((event.loaded * 100) / event.total);
-      };
-
-      // Once the content has been read, it's sent to the service
-      // for parsing.
-      reader.onload = _ => {
-        this.injectionService.injectContent(this.lang.value, this.type.value, reader.result.toString())
-          .subscribe(
-            next => {
-              this.contentValidationInProgress = next;
-            },
-            error => {
-              this.contentValidationInProgress = false;
-              console.error(`An error occured while parsing the content: ${error}`);
-            },
-            () => {
-              this.contentValidationInProgress = false;
-            });
-      };
-
-    }
-
-  }
-
-  cancelFile() {
-    // Stop the parsing of the content
-    this.injectionService.cancelContentInjection();
-    // No file uploaded, hence empty array
-    this.files = [];
-    // No parsing running
-    this.contentValidationInProgress = false;
-  }
 }
 
 class ContentUploadForm {
   public static readonly LANG = 'lang';
-  public static readonly TYPE = 'type';
+  public static readonly ANNOTATION = 'annotation';
 
   lang = new FormControl();
-  type = new FormControl();
+  annotation = new FormControl();
 
-  constructor(type?: string, lang?: string) {
-    if (type) {
-      this.type.setValue(type);
+  constructor(annotation?: string, lang?: string) {
+    if (annotation) {
+      this.annotation.setValue(annotation);
     }
     if (lang) {
       this.lang.setValue(lang);
@@ -152,7 +146,7 @@ class ContentUploadForm {
   }
 }
 
-export class FileUploadModel {
+class FileUploadModel {
   data: File;
   state: string;
   progress: number;
